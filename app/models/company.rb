f75b61fef43d900b46e259078056de70cdaa2474
unknown <Administrator@celab2-15.comp1-domain.spit.ac.in>
2012-09-16 15:27:10 +0530

    update

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,9 @@
 class Company < ActiveRecord::Base
+  validate:symbol_should_be_3_or_4_letters
+  def symbol_should_be_3_or_4_letters
+    if symbol.length>4
+        errors.add :symbol,'should be 3 or 4 letters'
+
+    end
+  end
 end
--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,2 +1,9 @@
 class Investment < ActiveRecord::Base
+  validate :quantity_should_be_positive
+  def quantity_should_be_positive
+    if quantity < 0
+      errors.add :quantity,'should be positive'
+    end
+  end
+
 end
